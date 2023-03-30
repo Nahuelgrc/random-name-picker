1ea463c47a53b137d3c25cd761cdd2c77c34e224
Nahue <nahuelgrc@gmail.com>
2023-03-30 20:11:14 -0300

    Add new icon

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -1,8 +1,8 @@
 export const getGifUrlByString = async (name = "") => {
+  const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${
-      import.meta.env.VITE_GIPHY_API_KEY
-    }&q=${name}`
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${name}`
   );
   const { data } = await response.json();
   return data;
