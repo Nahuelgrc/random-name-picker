7f09f74ec5cbb64c3f82e8987dc20156099e7441
Nahue <nahuelgrc@gmail.com>
2023-04-01 19:48:11 -0300

    Show remove button when mouse cursor on line

--- src/sections/ListNames.jsx
+++ src/sections/ListNames.jsx
@@ -1,9 +1,11 @@
 import { Button, Stack, Text, Heading, Flex, Box } from "@chakra-ui/react";
 import { useAppProvider } from "../provider/AppProvider";
 import { MinusIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 export const ListNames = () => {
   const { list, cleanList, removeElement } = useAppProvider();
+  const [indexButtonColor, setIndexButtonColor] = useState({});
 
   return (
     <Stack w="full" h="full" p={5} spacing={5} alignItems="center">
@@ -13,24 +15,38 @@ export const ListNames = () => {
       ) : (
         <Stack w="275px" alignItems="center">
           <Stack mb={1} w="full">
-            {list.map((name, index) => (
-              <Flex key={index} justifyContent="space-between">
-                <Stack>
-                  <Text>{name}</Text>
-                </Stack>
-                <Stack justifyContent="end">
-                  <Button
-                    onClick={() => {
-                      removeElement(name);
-                    }}
-                    size="xs"
-                    colorScheme="red"
-                  >
-                    <MinusIcon />
-                  </Button>
-                </Stack>
-              </Flex>
-            ))}
+            {list.map((name, index) => {
+              return (
+                <Flex
+                  key={index}
+                  justifyContent="space-between"
+                  onMouseEnter={() => {
+                    setIndexButtonColor({ ...indexButtonColor, [index]: true });
+                  }}
+                  onMouseLeave={() => {
+                    setIndexButtonColor({
+                      ...indexButtonColor,
+                      [index]: false,
+                    });
+                  }}
+                >
+                  <Stack>
+                    <Text>{name}</Text>
+                  </Stack>
+                  <Stack justifyContent="end">
+                    <Button
+                      onClick={() => {
+                        removeElement(name);
+                      }}
+                      size="xs"
+                      colorScheme={indexButtonColor[index] ? "red" : "white"}
+                    >
+                      <MinusIcon />
+                    </Button>
+                  </Stack>
+                </Flex>
+              );
+            })}
           </Stack>
 
           <Button onClick={cleanList} colorScheme="red" w="full">
